b74c0f77c6b5b33969af09b8659710f4ad00e8b0
abdulladone94 <abdullaf@booleanlabs.biz>
2023-10-02 13:03:09 +0530

    created home page and mapped task data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/Home";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Home />
     </div>
   );
 }
